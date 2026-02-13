8d66eebfffeefd218c9a8cc24aa92ef95679a27c
Maryam Sherif <mariamsherif2.ms@gmail.com>
2026-02-13 15:21:03 +0000

    Refactor code structure for improved readability and maintainability

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.maryamsherif.sunnahtracker',
-  appName: 'SunnahTracker',
-  webDir: 'dist'
-};
-
-export default config;
